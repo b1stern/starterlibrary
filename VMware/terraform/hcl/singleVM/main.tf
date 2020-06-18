0f259688374997e475977c81f6155ef6fdb29538
Ben Stern <bstern@us.ibm.com>
2020-06-18 16:31:45 -0400

    Update main.tf

--- VMware/terraform/hcl/singleVM/main.tf
+++ VMware/terraform/hcl/singleVM/main.tf
@@ -66,7 +66,7 @@ variable "vm_1_name" {
 
 variable "vm_1_folder" {
   description = "Target vSphere folder for virtual machine"
-  default     = "/teraform/singlevm"
+  default     = "/RTP/vm/teraform"
 }
 
 variable "vm_1_datacenter" {
